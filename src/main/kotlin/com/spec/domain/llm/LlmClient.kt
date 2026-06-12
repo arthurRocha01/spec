f3ad619f0dd152e887262a1a7cecbd05b70fb3b2
Arthur Rocha <arthurrocha007.net@gmail.com>
2026-06-12 13:04:45 -0300

    implements LLM client

--- src/main/kotlin/com/spec/domain/llm/LlmClient.kt
+++ src/main/kotlin/com/spec/domain/llm/LlmClient.kt
@@ -3,5 +3,5 @@ package com.spec.domain.llm
 import com.spec.domain.product.Product
 
 interface LlmClient {
-    suspend fun synthesize(request: SynthesisRequest): Product
+    suspend fun synthesize(rawText: String): Product
 }
